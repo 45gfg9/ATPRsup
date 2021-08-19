435816f9ec26c15ad37fb46c50b866fa4a41307e
45gfg9 <mc45@qq.com>
2021-08-20 02:30:23 +0800

    Use `Rusb` crate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-extern crate libusb;
-
-// Rust libusb example code.
+// Rusb example code.
 fn main() {
-    let context = libusb::Context::new().unwrap();
-
-    for device in context.devices().unwrap().iter() {
+    for device in rusb::devices().unwrap().iter() {
         let device_desc = device.device_descriptor().unwrap();
 
-        println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
-                 device.bus_number(),
-                 device.address(),
-                 device_desc.vendor_id(),
-                 device_desc.product_id());
+        println!(
+            "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+            device.bus_number(),
+            device.address(),
+            device_desc.vendor_id(),
+            device_desc.product_id()
+        );
     }
 }
