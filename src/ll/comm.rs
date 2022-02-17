3cbc31ffef41c0dc341ca68a052882cf537afa32
45gfg9 <mc45@qq.com>
2022-02-17 15:07:59 +0800

    Pack Version tuple struct

--- src/ll/comm.rs
+++ src/ll/comm.rs
@@ -4,6 +4,9 @@ use rusb::{DeviceHandle, Result};
 
 use super::ATPRContext;
 
+#[derive(Debug)]
+pub struct Version(u8, u8, u8);
+
 pub struct ATPR {
     handle: DeviceHandle<ATPRContext>,
 }
@@ -23,7 +26,7 @@ impl ATPR {
         &self.handle
     }
 
-    pub fn version(&self) -> Result<(u8, u8, u8)> {
+    pub fn version(&self) -> Result<Version> {
         let stamp: u8 = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap()
@@ -41,7 +44,7 @@ impl ATPR {
             )
             .map(|bytes| {
                 assert_eq!((4, stamp), (bytes, buf[0]), "Wrong response from firmware");
-                (buf[1], buf[2], buf[3])
+                Version(buf[1], buf[2], buf[3])
             })
     }
 }
